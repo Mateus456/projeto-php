84743d373667e1853b23c563d1b64af124e2849e
Mateus Lima <oliveiralimamateus92@gmail.com>
2018-06-21 21:07:13 -0300

    Inicio Projeto PHP

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+	$titulo = "Título da página";
+	$tituloConteudo = "<h1>Titulo do Texto</h1>";
+	$conteudo = "<p>GIT - Versionamento</p>";
+?>
+
+<html>
+<head>
+	<title><?php echo $titulo; ?></title>
+	<meta charset="utf-8"/>
+</head>
+<body>
+<?php
+	echo $tituloConteudo;
+	echo $conteudo;
+?>
+</body>
+</html>	
